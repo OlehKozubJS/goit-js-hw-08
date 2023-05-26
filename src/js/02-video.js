e1f346156857be209b0c0ade33626d95cfdf653d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-05-27 01:13:16 +0300

    Update All

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on("timeupdate", timeUpdate);
+player.on("timeupdate", throttle(timeUpdate, 1000));
+
+function timeUpdate (event) {
+    console.log(event);
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
 
