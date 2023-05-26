fee221c9ef39d07c10aca27c3cd8a61737606bd4
OlehKozubJS <oleg.kozub54@gmail.com>
2023-05-26 23:01:23 +0300

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+let galleryString = "";
+
+galleryItems.forEach(image => {
+    galleryString +=   
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>
+    </li>`;
+});
+
+gallery.innerHTML = galleryString;
+
+let instance = new SimpleLightbox('.gallery a', 
+    {
+        captionsData: "alt",
+        captionDelay: 250,
+        disableScroll: false,
+    }
+);
