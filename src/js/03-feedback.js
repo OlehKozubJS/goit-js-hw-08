db522c4ae65d7e866f1dccf590ca2ff1826af3bb
OlehKozubJS <oleg.kozub54@gmail.com>
2023-05-27 02:53:59 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
 import throttle from "lodash.throttle";
+
+const feedbackForm = document.querySelector(".feedback-form");
+
+window.addEventListener("load", readMessage);
+feedbackForm.addEventListener("input", writeMessage);
+feedbackForm.addEventListener("input", throttle(writeMessage, 500));
+feedbackForm.addEventListener("submit", submitForm);
+
+let feedbackFormState = JSON.parse(localStorage.getItem("feedback-form-state")) || {email: "", message: ""};
+
+function readMessage() {
+    document.querySelector("input").value = feedbackFormState.email;
+    document.querySelector("textarea").value = feedbackFormState.message;
+}
+
+function writeMessage(event) {
+    if (event.target.nodeName === "INPUT") {
+        feedbackFormState.email = event.target.value;
+    }
+
+    if (event.target.nodeName === "TEXTAREA") {
+        feedbackFormState.message = event.target.value;
+    }
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(feedbackFormState));
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    feedbackFormState = JSON.parse(localStorage.getItem("feedback-form-state"));
+    console.log(feedbackFormState);
+    event.currentTarget.reset();
+}
